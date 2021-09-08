2c41ab9da050eebc88c15783efeae01ad725710f
devStudent <82213729+devStudent@users.noreply.github.com>
2021-09-08 19:50:16 +0200

    Practicas

---
+++ 04-Week/script.js
@@ -0,0 +1,11 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+var letra;
+for (var i=0; i < names.length; i++) {  
+  letra=names[i].charAt(0).toLowerCase();    
+  if (letra==='j') {
+    speakGoodBye.speak(names[i]);
+  } else {
+    speakHello.speak(names[i]); 
+  }
+}
